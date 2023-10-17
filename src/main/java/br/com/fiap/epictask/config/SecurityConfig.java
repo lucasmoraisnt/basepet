b5c26367b2472994a9ab2907e6cba6a6db40b868
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-16 22:12:03 -0300

    Login

--- src/main/java/br/com/fiap/epictask/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/epictask/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package main.java.br.com.fiap.epictask.config;
+package br.com.fiap.epictask.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +8,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
         return http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .oauth2Login(Customizer.withDefaults())
-            .formLogin(Customizer.withDefaults())
-            .build();
-
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login( form -> form.loginPage("/login").defaultSuccessUrl("/task").permitAll())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login") )
+                .build();
     }
 
 }
