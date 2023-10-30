f2a1c4a8ae81cea3cc81d50b1609ee96553055a1
ValentinaFuzaro <rm93267@fiap.com.br>
2023-10-30 13:05:38 -0300

    Apagando projeto que não foi informado que era para ser diferente

--- src/main/java/br/com/fiap/epictask/task/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.epictask.task;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long>{
-    
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TASK (title, descricao, score) VALUES ('Prototipo', 'Criar no figma', 50);
-INSERT INTO TASK (title, descricao, score, status) VALUES ('BD', 'Modelar o banco de dados', 50, 30);
-INSERT INTO TASK (title, descricao, score) VALUES ('Testes', 'Fazer os testes unitarios', 50);
