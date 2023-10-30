068129b6486a1556402a61e7d21c37931abf7379
ValentinaFuzaro <rm93267@fiap.com.br>
2023-10-30 13:10:34 -0300

    Esqueleto do projeto

---
+++ src/main/java/br/com/fiap/basepet/BasepetApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.basepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasepetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/basepet/BasepetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.basepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasepetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
